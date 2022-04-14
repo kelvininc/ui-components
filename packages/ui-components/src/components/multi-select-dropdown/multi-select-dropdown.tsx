79c47cdd377fd1c8df694a4b16bb1aa8064a81fa
Daniel Carvalho <mail@danielcarvalho.pt>
2022-04-14 17:06:58 +0100

    feat(dropdown): add prop for search reset

--- packages/ui-components/src/components/multi-select-dropdown/multi-select-dropdown.tsx
+++ packages/ui-components/src/components/multi-select-dropdown/multi-select-dropdown.tsx
@@ -52,6 +52,7 @@ export class KvMultiSelectDropdown implements IMultiSelectDropdown, IMultiSelect
 	@Event() selectionCleared: EventEmitter<void>;
 
 	@State() _selectionDisplayValue: string;
+	@State() _searchValue: string;
 
 	private selectOption = (event: CustomEvent<string>) => {
 		const option = event.detail;
@@ -85,9 +86,15 @@ export class KvMultiSelectDropdown implements IMultiSelectDropdown, IMultiSelect
 
 	private openStateChangeHandler = (event: CustomEvent<boolean>) => {
 		this.isOpen = event.detail;
+
+		if (!this.isOpen) {
+			this._searchValue = '';
+			this.searchChange.emit('');
+		}
 	};
 
 	private onSearchChange = (event: CustomEvent<string>) => {
+		this._searchValue = event.detail;
 		this.searchChange.emit(event.detail);
 	};
 
@@ -136,6 +143,7 @@ export class KvMultiSelectDropdown implements IMultiSelectDropdown, IMultiSelect
 				>
 					<kv-dropdown-list
 						searchable={this.searchable}
+						searchValue={this._searchValue}
 						selectionClearable={!isEmpty(this.options) && this.selectionClearable}
 						selectionClearEnabled={this.selectedOptions.length > 0}
 						clearSelectionLabel={this.clearSelectionLabel}
