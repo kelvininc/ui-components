14d99a081b8e74724bdc645bf51ed55292409cfd
Diogo Couto <diogo2couto@gmail.com>
2022-07-11 18:56:33 +0100

    feat(tree-item): change tree-item to be an anchor element

--- packages/ui-components/src/components/tree/tree.tsx
+++ packages/ui-components/src/components/tree/tree.tsx
@@ -33,6 +33,10 @@ export class KvTree {
 	@Event() nodeToggleExpand: EventEmitter<ITreeNodeItem>;
 	/** Emitted when the tree node item is clicked */
 	@Event() nodeClick: EventEmitter<ITreeNodeItem>;
+	/** Emitted when the expand toggle is clicked */
+	@Event() toggleExpand: EventEmitter<MouseEvent>;
+	/** Emitted when the tree item is clicked */
+	@Event() itemClick: EventEmitter<MouseEvent>;
 
 	private onItemClick = (item: ITreeNodeItem) => this.nodeClick.emit(item);
 
@@ -54,9 +58,13 @@ export class KvTree {
 								counter={item.counter}
 								counterState={item.counterState}
 								hasChildren={item.lazyLoadChildren}
+								href={item.href}
+								target={item.target}
+								download={item.download}
 								selected={this.selectedNode === item.id}
 								expanded={get(this.expandedNodes, [item.id], false)}
 								disabled={get(this.disabledNodes, [item.id], false)}
+								preventDefault={item.preventDefault}
 								highlighted={get(this.highlightedNodes, [item.id], false)}
 								loading={this.loading || get(this.loadingNodes, [item.id], false)}
 								onItemClick={_ => this.onItemClick(item)}
