e24c4352ef59f2b9dadcb4184ebd152339a2a305
Luis Araújo <44007026+laraujo7@users.noreply.github.com>
2022-09-05 19:22:04 +0100

    refactor(range): Refactor range & fix range in firefox (#135)

--- packages/ui-components/src/components/range/range.helper.ts
+++ packages/ui-components/src/components/range/range.helper.ts
@@ -1,8 +1,10 @@
-export const getInputPercentageFromValue = (inputValue: string, min: number, max: number) => {
-	return (100 * (parseInt(inputValue) - min)) / (max - min);
+export const getInputPercentageFromValue = (inputValue: number, min: number, max: number) => {
+	return (100 * (inputValue - min)) / (max - min);
 };
 
 export const getOffset = (percentage: number) => {
-	const invertPercentage = 1 - percentage / 100;
-	return invertPercentage * 10 - 4;
+	const offSet = -0.25;
+	const halfDistance = percentage - 50;
+
+	return halfDistance * offSet;
 };
