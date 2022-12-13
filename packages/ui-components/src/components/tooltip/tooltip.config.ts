ce700e4a569025b7f9c3ca9e43066adf697f0eb1
Luis Araújo <l.araujoo.1996@gmail.com>
2022-12-13 20:10:02 +0000

    feat(tooltip): add default delay to tooltip

--- packages/ui-components/src/components/tooltip/tooltip.config.ts
+++ packages/ui-components/src/components/tooltip/tooltip.config.ts
@@ -4,3 +4,5 @@ export const DEFAULT_POSITION_CONFIG: Partial<ComputePositionConfig> = {
 	strategy: 'fixed',
 	middleware: [offset(5), shift({ padding: 5 })]
 };
+
+export const DEFAULT_DELAY_CONFIG: number = 1000;
