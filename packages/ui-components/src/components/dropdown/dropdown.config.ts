1e1509e5761597c78412510e5f54cce52590c748
Diogo Couto <diogo2couto@gmail.com>
2023-03-08 10:29:09 +0000

    fix(dropdown): add missing flip middleware

--- packages/ui-components/src/components/dropdown/dropdown.config.ts
+++ packages/ui-components/src/components/dropdown/dropdown.config.ts
@@ -1,4 +1,4 @@
-import { ComputePositionConfig, MiddlewareState, offset, size } from '@floating-ui/dom';
+import { ComputePositionConfig, MiddlewareState, offset, size, flip } from '@floating-ui/dom';
 import { EInputFieldType, EValidationState, ITextField } from '../text-field/text-field.types';
 
 export const DROPDOWN_DEFAULT_PLACEHOLDER = 'Select an option';
@@ -15,6 +15,10 @@ export const DEFAULT_DROPDOWN_POSITION_CONFIG: Partial<ComputePositionConfig> = 
 	placement: 'bottom',
 	middleware: [
 		offset(DEFAULT_DROPDOWN_OFFSET),
+		flip({
+			padding: 16,
+			fallbackPlacements: ['top-end', 'bottom-end', 'top-start', 'bottom-start']
+		}),
 		size({
 			apply({ rects, elements }: MiddlewareState) {
 				Object.assign(elements.floating.style, {
