f8bd0c34d004fc1772054de0caec6fb3092fea6f
Pedro Costa <pedro.costa@kelvininc.com>
2023-06-20 17:40:11 +0100

    feat(select): add header actions slot

--- packages/ui-components/src/components/select/select.tsx
+++ packages/ui-components/src/components/select/select.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h, Prop, Event, EventEmitter } from '@stencil/core';
+import { Component, Host, h, Prop, Event, EventEmitter, Element } from '@stencil/core';
 import { isNil, omitBy } from 'lodash-es';
 import { SELECT_CLEAR_SELECTION_LABEL } from './select.config';
 import { ISelect, ISelectEvents } from './select.types';
@@ -34,6 +34,8 @@ export class KvSelect implements ISelect, ISelectEvents {
 	/** @inheritdoc */
 	@Event() clearSelection: EventEmitter<void>;
 
+	@Element() el: HTMLKvSelectElement;
+
 	private onSearchChange = (event: CustomEvent<string>) => {
 		this.searchChange.emit(event.detail);
 	};
@@ -42,22 +44,25 @@ export class KvSelect implements ISelect, ISelectEvents {
 		this.clearSelection.emit();
 	};
 
-	private getCustomStyle = () =>
-		omitBy(
+	private get customStyle() {
+		return omitBy(
 			{
 				'--select-max-height': this.maxHeight,
 				'--select-min-height': this.minHeight
 			},
 			isNil
 		);
+	}
+
+	private get hasActionsSlot() {
+		return !isNil(this.el.querySelector('[slot="select-header-actions"]'));
+	}
 
 	render() {
-		const customStyle = this.getCustomStyle();
-
 		return (
-			<Host style={customStyle}>
+			<Host style={this.customStyle}>
 				<div class="select-container" part="select">
-					{(this.searchable || this.selectionClearable) && (
+					{(this.searchable || this.selectionClearable || this.hasActionsSlot) && (
 						<div class="select-header-container">
 							{this.searchable && <kv-search value={this.searchValue} placeholder={this.searchPlaceholder} onTextChange={this.onSearchChange} />}
 							{this.selectionClearable && (
@@ -71,6 +76,7 @@ export class KvSelect implements ISelect, ISelectEvents {
 									{this.clearSelectionLabel}
 								</div>
 							)}
+							<slot name="select-header-actions" />
 						</div>
 					)}
 					<div class="select-options-container">
