c3fe223ef40b395db4e5c30b7e997cffaf186f14
Diogo Couto <diogo2couto@gmail.com>
2023-10-02 11:44:27 +0100

    feat(search): add missing `kv-text-field` props to `kv-search`

--- packages/ui-components/src/components/search/search.types.ts
+++ packages/ui-components/src/components/search/search.types.ts
@@ -1,6 +1,9 @@
 import { EventEmitter } from '@stencil/core';
+import { ITextField, ITextFieldEvents } from '../text-field/text-field.types';
 
-export interface ISearchEvents {
+export interface ISearch extends Omit<ITextField, 'icon' | 'actionIcon'> {}
+
+export interface ISearchEvents extends ITextFieldEvents {
 	/** Emitted when the reset buccon is clicked */
 	clickResetButton: EventEmitter<MouseEvent>;
 }
