a6615ea39caeac4513d38f9a40286aeddb57f9c8
GoncaloCamaz <gcamaz@sapo.pt>
2023-12-05 11:47:14 +0000

    fix(dropdowns): change dropdown search input size to small

--- packages/ui-components/src/components/select-multi-options/select-multi-options.config.ts
+++ packages/ui-components/src/components/select-multi-options/select-multi-options.config.ts
@@ -1,3 +1,3 @@
 export const DEFAULT_NO_DATA_AVAILABLE_LABEL = 'No data available.';
-export const MINIMUM_SEARCHABLE_OPTIONS = 6;
+export const MINIMUM_SEARCHABLE_OPTIONS = 15;
 export const SELECT_OPTION_HEIGHT_IN_PX = 32;
