aa5505e4afb6975505df79675c88f9e123777773
ricardo.goncalves <ricardo.goncalves@kelvininc.com>
2024-02-23 01:59:56 +0000

    feat(schema-form): add the radio list widget

--- packages/react-ui-components/src/components/SchemaForm/Widgets/index.ts
+++ packages/react-ui-components/src/components/SchemaForm/Widgets/index.ts
@@ -9,10 +9,12 @@ import TextareaWidget from './TextareaWidget';
 import FileWidget from './FileWidget';
 
 import { StrictRJSFSchema, RJSFSchema, FormContextType, RegistryWidgetsType } from '@rjsf/utils';
+import RadioListWidget from './RadioListWidget';
 
 export function generateWidgets<T = any, S extends StrictRJSFSchema = RJSFSchema, F extends FormContextType = any>(): RegistryWidgetsType<T, S, F> {
 	return {
 		RadioWidget,
+		RadioListWidget,
 		SelectWidget,
 		CheckboxesWidget,
 		readOnlyValue: ReadOnlyValueWidget,
