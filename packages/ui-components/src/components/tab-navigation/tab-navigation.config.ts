92ee165c5802539cf8b684bab62ab7d8670da4b7
Diogo Couto <diogo2couto@gmail.com>
2025-01-13 13:58:56 +0000

    feat(tab-item): enable icon style customization

--- packages/ui-components/src/components/tab-navigation/tab-navigation.config.ts
+++ packages/ui-components/src/components/tab-navigation/tab-navigation.config.ts
@@ -7,4 +7,5 @@ export const FONT_DESCRIPTOR_SIZE_MAP: Record<EComponentSize, string> = {
 
 export const TAB_ITEM_PADDING_PX = 48;
 export const TAB_ITEM_NOTIFICATION_DOT_WITH_MARGIN = 14;
+export const TAB_ITEM_ICON_WIDTH_PX = 20;
 export const TAB_ITEM_SMALL_ADDED_MARGIN = 32;
