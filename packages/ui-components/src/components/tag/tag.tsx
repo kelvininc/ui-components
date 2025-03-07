1554e9645d60e8719f3d8d56ecda533a0b052148
ricardo.goncalves <ricardo.goncalves@kelvininc.com>
2025-03-07 18:55:36 +0000

    feat(tag): add size prop

--- packages/ui-components/src/components/tag/tag.tsx
+++ packages/ui-components/src/components/tag/tag.tsx
@@ -1,5 +1,6 @@
 import { Component, Host, Prop, h } from '@stencil/core';
 import { isValidLabel } from '../../utils/string.helper';
+import { EComponentSize } from '../../types';
 
 @Component({
 	tag: 'kv-tag',
@@ -9,11 +10,13 @@ import { isValidLabel } from '../../utils/string.helper';
 export class KvTag {
 	/** (optional) Tag label */
 	@Prop({ reflect: true }) label?: string;
+	/** (optional) Sets the size of tag */
+	@Prop({ reflect: true }) size: EComponentSize = EComponentSize.Large;
 
 	render() {
 		return (
 			<Host>
-				<div class="tag-container">
+				<div class={{ 'tag-container': true, [`tag-container--size-${this.size}`]: true }}>
 					<slot name="left-slot"></slot>
 					{isValidLabel(this.label) && <div class="tag-label">{this.label}</div>}
 				</div>
