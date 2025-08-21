c4da8d41bd3b674221c1d8b7896d0f5af9b76dc8
Diogo Couto <diogo2couto@gmail.com>
2025-08-21 17:22:31 +0100

    feat: restructure `@kelvininc/react-ui-components` for Next.js SSR compatibility

---
+++ packages/react-ui-components/src/client.ts
@@ -0,0 +1,11 @@
+'use client';
+
+// Export all client-side Stencil components
+export * from './stencil-generated';
+
+// Export client-side specific components and hooks
+export * from './components';
+export * from './hooks';
+
+// Export shared utils and types
+export * from './shared';
