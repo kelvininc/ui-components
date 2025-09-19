37d703c052ecde3540a803585b3bb4bdf8d7b67d
Diogo Couto <diogo2couto@gmail.com>
2025-09-19 19:28:06 +0100

    fix(schema-form): normalize `oneOf` schemas to avoid exceeding maximum call stack size

--- packages/react-ui-components/src/components/SchemaForm/Widgets/SelectWidget/utils.ts
+++ packages/react-ui-components/src/components/SchemaForm/Widgets/SelectWidget/utils.ts
@@ -1,9 +1,39 @@
-import { asNumber, guessType } from '@rjsf/utils';
+import { asNumber, guessType, RJSFSchema, StrictRJSFSchema } from '@rjsf/utils';
 import { JSONSchema7 } from 'json-schema';
 import { get, isEmpty, isNil, isString } from 'lodash';
 import { EnumOptions, IUIDropdownOptions } from './types';
 
 const numericTypes = ['number', 'integer'];
+
+const enumSchemaHasCustomTitles = (schema: unknown): schema is { 'x-titles': string[] } => {
+	if (!schema || typeof schema !== 'object' || Array.isArray(schema)) {
+		return false;
+	}
+
+	return 'x-titles' in schema;
+};
+
+/**
+ * Retrieves the label for a given schema and index, using custom titles if available.
+ *
+ * @typeParam S - Schema type extending `StrictRJSFSchema`.
+ * @param {Object} params
+ * @param {S} params.schema - The schema to retrieve the label from.
+ * @param {number} params.index - The index of the label to retrieve.
+ * @param {string} params.fallback - The fallback label to use if no custom title is found.
+ * @returns {string} The label for the given schema and index.
+ */
+const getEnumLabel = <S extends StrictRJSFSchema = RJSFSchema>({ schema, index, fallback }: { schema: S; index: number; fallback: string }): string => {
+	if (schema.type === 'array' && enumSchemaHasCustomTitles(schema.items)) {
+		return schema.items['x-titles'][index] || fallback;
+	}
+
+	if (enumSchemaHasCustomTitles(schema)) {
+		return schema['x-titles'][index] || fallback;
+	}
+
+	return fallback;
+};
 
 /**
  * This is a silly limitation in the DOM where option change event values are
@@ -45,15 +75,29 @@ export const buildSelectedOptions = (selectedOptions: string[]): Record<string, 
 		return accumulator;
 	}, {});
 
-export const buildDropdownOptions = (options?: EnumOptions, disabledOptions?: EnumOptions, multiSubOptions?: IUIDropdownOptions): IUIDropdownOptions => {
+export const buildDropdownOptions = <S extends StrictRJSFSchema = RJSFSchema>({
+	schema,
+	options,
+	disabledOptions,
+	multiSubOptions
+}: {
+	schema: S;
+	options?: EnumOptions;
+	disabledOptions?: EnumOptions;
+	multiSubOptions?: IUIDropdownOptions;
+}): IUIDropdownOptions => {
 	if (!isEmpty(multiSubOptions)) {
 		return multiSubOptions;
 	}
 
 	return Array.isArray(options)
-		? options.reduce((acc, { label, value, schema }) => {
-				const description = schema?.description;
+		? options.reduce((acc, { label: optionLabel, value, schema: optionSchema }, index) => {
+				const description = optionSchema?.description;
 				const disabled = Array.isArray(disabledOptions) && disabledOptions.indexOf(value) != -1;
+
+				// If the schema has custom titles, use them instead of the enum options
+				let label = getEnumLabel({ schema, index, fallback: optionLabel });
+
 				acc[value] = { value, label, description, disabled };
 				return acc;
 		  }, {})
