963f5ee5ecd67080ba67b03e05fc2bc14585081a
Diogo Couto <diogo2couto@gmail.com>
2025-10-15 17:06:14 +0100

    fix(dropdown): remove helptext height offset

---
+++ packages/ui-components/src/components/dropdown/dropdown.helper.ts
@@ -0,0 +1,30 @@
+import { ComputePositionConfig, flip, MiddlewareState, offset, size } from '@floating-ui/dom';
+import { isEmpty } from 'lodash-es';
+import { DEFAULT_DROPDOWN_OFFSET, HELP_TEXT_HEIGHT_PX } from './dropdown.config';
+import { ITextField } from '../text-field/text-field.types';
+
+export const getDefaultDropdownPositionConfig = (
+	inputConfig: Partial<ITextField> & {
+		inputDisabled: boolean;
+	}
+): Partial<ComputePositionConfig> => {
+	const helpTextOffset = isEmpty(inputConfig.helpText) ? 0 : HELP_TEXT_HEIGHT_PX;
+
+	return {
+		placement: 'bottom',
+		middleware: [
+			offset(DEFAULT_DROPDOWN_OFFSET - helpTextOffset),
+			flip({
+				padding: 16,
+				fallbackPlacements: ['top-end', 'bottom-end', 'top-start', 'bottom-start']
+			}),
+			size({
+				apply({ rects, elements }: MiddlewareState) {
+					Object.assign(elements.floating.style, {
+						width: `${rects.reference.width}px`
+					});
+				}
+			})
+		]
+	};
+};
