18f9a00e990f23b9758c98740a242e82f93414bf
ricardo.goncalves <ricardo.goncalves@kelvininc.com>
2025-11-21 18:21:31 +0000

    fix(schema-form): fix oneof titles [KFE-2686]

--- packages/react-ui-components/src/components/SchemaForm/Widgets/SelectWidget/utils.ts
+++ packages/react-ui-components/src/components/SchemaForm/Widgets/SelectWidget/utils.ts
@@ -5,36 +5,6 @@ import { EnumOptions, IUIDropdownOptions } from './types';
 
 const numericTypes = ['number', 'integer'];
 
-const enumSchemaHasCustomTitles = (schema: unknown): schema is { 'x-titles': string[] } => {
-	if (!schema || typeof schema !== 'object' || Array.isArray(schema)) {
-		return false;
-	}
-
-	return 'x-titles' in schema;
-};
-
-/**
- * Retrieves the label for a given schema and index, using custom titles if available.
- *
- * @typeParam S - Schema type extending `StrictRJSFSchema`.
- * @param {Object} params
- * @param {S} params.schema - The schema to retrieve the label from.
- * @param {number} params.index - The index of the label to retrieve.
- * @param {string} params.fallback - The fallback label to use if no custom title is found.
- * @returns {string} The label for the given schema and index.
- */
-const getEnumLabel = <S extends StrictRJSFSchema = RJSFSchema>({ schema, index, fallback }: { schema: S; index: number; fallback: string }): string => {
-	if (schema.type === 'array' && enumSchemaHasCustomTitles(schema.items)) {
-		return schema.items['x-titles'][index] || fallback;
-	}
-
-	if (enumSchemaHasCustomTitles(schema)) {
-		return schema['x-titles'][index] || fallback;
-	}
-
-	return fallback;
-};
-
 /**
  * This is a silly limitation in the DOM where option change event values are
  * always retrieved as strings.
@@ -91,12 +61,9 @@ export const buildDropdownOptions = <S extends StrictRJSFSchema = RJSFSchema>({
 	}
 
 	return Array.isArray(options)
-		? options.reduce((acc, { label: optionLabel, value, schema: optionSchema }, index) => {
+		? options.reduce((acc, { label, value, schema: optionSchema }, index) => {
 				const description = optionSchema?.description;
 				const disabled = Array.isArray(disabledOptions) && disabledOptions.indexOf(value) != -1;
-
-				// If the schema has custom titles, use them instead of the enum options
-				let label = getEnumLabel({ schema, index, fallback: optionLabel });
 
 				acc[value] = { value, label, description, disabled };
 				return acc;
