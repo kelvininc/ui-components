46dd6c0ead8cb8dd8dd7ea5f1ff9e82589592446
ricardo.goncalves <ricardo.goncalves@kelvininc.com>
2026-04-08 17:07:20 +0100

    feat(code-editor): fix light dark modes

--- packages/react-ui-components/src/components/CodeEditor/hooks/useLoadMonacoEditorStyle.ts
+++ packages/react-ui-components/src/components/CodeEditor/hooks/useLoadMonacoEditorStyle.ts
@@ -1,20 +1,37 @@
 import { useMonaco } from '@monaco-editor/react';
-import { CodeDiffEditorOptions, CodeEditorOptions, ECodeEditorTheme } from '../types';
+import { CodeDiffEditorOptions, CodeEditorOptions, CodeEditorTheme, ECodeEditorTheme } from '../types';
 import { getFontOptions } from '../utils';
 import { useFontsApi } from '../../../hooks';
 import { useCallback, useEffect, useMemo } from 'react';
 import { editor } from 'monaco-editor';
+import { DEFAULT_KELVIN_CODE_EDITOR_THEME } from '../config';
 
-export const useLoadMonacoEditorStyle = (options: CodeDiffEditorOptions | CodeEditorOptions, customTheme: editor.IStandaloneThemeData) => {
+export const useLoadMonacoEditorStyle = (options: CodeDiffEditorOptions | CodeEditorOptions, theme: ECodeEditorTheme, customTheme = DEFAULT_KELVIN_CODE_EDITOR_THEME) => {
 	const monaco = useMonaco();
 	const { isFontLoaded, loadFont } = useFontsApi(getFontOptions(options));
 
 	const hasLoaded = useMemo(() => monaco && isFontLoaded, [monaco, isFontLoaded]);
 
 	const defineCustomTheme = useCallback(() => {
-		if (!monaco || !customTheme) return;
-		monaco.editor.defineTheme(ECodeEditorTheme.Custom, customTheme);
-	}, [customTheme, monaco]);
+		if (!monaco) return;
+
+		let themeToDefine = customTheme;
+
+		if (theme !== ECodeEditorTheme.Custom) {
+			const isLightTheme = theme === ECodeEditorTheme.Light;
+			themeToDefine = {
+				base: theme,
+				inherit: true,
+				rules: [],
+				colors: {
+					'editor.background': !isLightTheme ? '#262626' : '#F2F2F2', // background/surface/neutral/subtle
+					'editorGutter.background': !isLightTheme ? '#1A1A1A' : '#FFFFFF' // background/surface/neutral/default
+				}
+			};
+		}
+
+		monaco.editor.defineTheme(ECodeEditorTheme.Custom, themeToDefine);
+	}, [theme, customTheme, monaco]);
 
 	useEffect(() => {
 		loadFont();
