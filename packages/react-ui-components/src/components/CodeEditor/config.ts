78c6df5ce9930ea0962f8c2275c7d8f7612e5a85
ricardo.goncalves <ricardo.goncalves@kelvininc.com>
2026-04-08 17:25:32 +0100

    feat(code-editor): migrate code editor to design tokens

--- packages/react-ui-components/src/components/CodeEditor/config.ts
+++ packages/react-ui-components/src/components/CodeEditor/config.ts
@@ -1,7 +1,6 @@
 import { CodeEditorTheme, ECodeEditorTheme, CodeEditorOptions } from './types';
 
 export const DEFAULT_CODE_EDITOR_LANGUAGE = 'yaml';
-export const DEFAULT_CODE_EDITOR_THEME = ECodeEditorTheme.Custom;
 
 export const DEFAULT_CODE_EDITOR_OPTIONS: CodeEditorOptions = {
 	readOnly: false,
@@ -10,13 +9,13 @@ export const DEFAULT_CODE_EDITOR_OPTIONS: CodeEditorOptions = {
 	}
 };
 
-export const KELVIN_CODE_EDITOR_THEME: CodeEditorTheme = {
+export const DEFAULT_KELVIN_CODE_EDITOR_THEME: CodeEditorTheme = {
 	base: 'vs-dark',
 	inherit: true,
 	rules: [],
 	colors: {
-		'editor.background': '#202020',
-		'editorGutter.background': '#2A2A2A'
+		'editor.background': '#262626',
+		'editorGutter.background': '#1A1A1A'
 	}
 };
 
